2cafb49dbe704ebfe47fb4ee1fccb56ebc4dafd6
mihiragrawal1 <mihiragrawal296@gmail.com>
2026-08-06 15:50:27 +0530

    basic framework setup and added login test scenarios

---
+++ pages/LoginPage.js
@@ -0,0 +1,32 @@
+import {expect} from "@playwright/test"
+
+
+export class LoginPage{
+
+    
+
+    constructor(page){
+        this.page = page;
+        this.username=page.locator("#email");
+        this.password=page.locator("#password");
+        this.signInButton=page.locator("#login-btn");
+        this.loginError=page.locator('[class="text-sm font-medium flex-1 leading-snug"]');
+        this.emptyPasswordErrorMsg= page.getByText('Password must be at least 6 characters')
+        this.emptyEmailErrorMsg= page.getByText('Enter a valid email');
+
+    }
+
+    async userLogin(username, password){
+        await this.username.fill(username);
+        await this.password.fill(password);
+        await this.signInButton.click();
+    }
+
+    async assertLoginError (){
+        await expect(this.loginError).toBeVisible();
+        await expect(this.loginError).toHaveText('Invalid email or password');
+
+    }
+
+
+}
---
+++ tests/LoginTest.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test'
+import { LoginPage } from '../pages/LoginPage'
+// import dotenv from 'dotenv';
+import {loginCreds,invalidLoginCreds} from '../testData/loginData'
+
+//username - testmihir@gmail.com and passwword is -Test@12345
+let loginPage;
+
+test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    await page.goto("/login")
+})
+
+test("Validate valid login with correct username and password", async ({ page }) => {
+    await loginPage.userLogin(loginCreds.username, loginCreds.password)
+    await expect(page).toHaveURL("/");
+})
+
+test("Validate login with incorrect username and password", async ({ page }) => {
+    await loginPage.userLogin(invalidLoginCreds.username,invalidLoginCreds.password)
+    await expect(page).toHaveURL("/login");
+    await loginPage.assertLoginError();
+
+})
+
+test("Validate login with empty username and password", async ({ page }) => {
+    await loginPage.userLogin("", "")
+    await expect(page).toHaveURL("/login");
+    await expect(loginPage.emptyEmailErrorMsg).toBeVisible();
+    await expect(loginPage.emptyPasswordErrorMsg).toBeVisible();
+})
